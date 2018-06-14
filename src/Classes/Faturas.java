edb6b41e678a389a84ebfc9f2ccf948778a288cb
Gabriel Almeida de Oliveira <gabriel-ao@hotmail.com>
2018-06-14 15:27:32 -0300

    get e set faturas

--- src/Classes/Faturas.java
+++ src/Classes/Faturas.java
@@ -5,12 +5,43 @@
  */
 package Classes;
 
-/**
- *
- * @author gabri
- */
+import java.util.Date;
+
 public class Faturas {
     
-
+    private Date dt_fatura;
+    private Date dt_venc;
+    private int id_tarifa;
+    private int id_usuario;
+    
+    public void setDt_fatura(Date dt_fatura){
+        this.dt_fatura = dt_fatura;
+    }
+    public Date getDt_fatura(){
+        return this.dt_fatura;
+    }
+    
+    
+    public void setDt_venc(Date dt_venc){
+        this.dt_venc = dt_venc;
+    }
+    public Date getDt_venc(){
+        return this.dt_venc;
+    }
+    
+    
+    public void setId_tarifa(int id_tarifa){
+        this.id_tarifa = id_tarifa;
+    }
+    public int getId_tarifa(){
+        return this.id_tarifa;
+    }
+    
+    public void setId_usuario(int id_usuario){
+        this.id_usuario = id_usuario;
+    }
+    public int getId_usuario(){
+        return this.id_usuario;
+    }
     
 }
